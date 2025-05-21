a217919f7db62b396833b8d303fa7b694f6d90c6
moltrus <moltrusweb@gmail.com>
2025-05-21 20:18:43 +0530

    fixed possible errors

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from typing import List
 from pydantic import BaseModel
 from transformers import pipeline
+import uvicorn
 
 app = FastAPI()
 
@@ -13,8 +14,11 @@ class BART_Large_MNLI(BaseModel):
 async def root():
    return {"message": "Server Running"}
 
-@app.post("facebook-bart-large-mnli")
+@app.post("/facebook-bart-large-mnli")
 async def bart_large_mnli(instance: BART_Large_MNLI) -> dict:
    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
    result = classifier(instance.text, instance.candidate_labels)
    return result
+
+if __name__ == "__main__":
+   uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
