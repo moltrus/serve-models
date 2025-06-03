188ac01705330b503f827441b1fb6255e86b4949
moltrus <moltrusweb@gmail.com>
2025-06-03 23:01:43 +0530

    added fb bart large mnli

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
+from transformers import pipeline
 import uvicorn
 import os
 
 app = FastAPI()
 
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+class BART_Large_MNLI(BaseModel):
+   text: str
+   candidate_labels: List[str] = []
+
 @app.get("/")
 async def root():
    return {"message": "Server Running"}
+
+@app.post("/facebook-bart-large-mnli")
+async def bart_large_mnli(instance: BART_Large_MNLI) -> dict:
+   result = classifier(instance.text, instance.candidate_labels)
+   return result
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
